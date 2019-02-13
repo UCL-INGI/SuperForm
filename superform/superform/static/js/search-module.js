518f1452f71e8c46256066a6f01b93c0cef984d2
AdriLezaack <adrien.lezaack@student.uclouvain.be>
2019-02-13 13:08:10 +0100

    Fichiers à surveiller, ils respectent mal la syntaxte de JS.

--- superform/superform/static/js/search-module.js
+++ superform/superform/static/js/search-module.js
@@ -1,3 +1,4 @@
+
 /*
 * waitJQuery : launch a function based on JQuery when JQuery is loaded
 * @params : func, the function to launch, args : its arguments
@@ -6,9 +7,9 @@
 
 function waitJQuery(func, args) {
     if (window.jQuery)
-        func(args);
+         func(args);
     else
-        setTimeout(function () {
+        setTimeout(function() {
             waitJQuery(func, args)
         }, 40);
 }
@@ -21,7 +22,7 @@ function waitJQuery(func, args) {
 function loadSearchTemplate(fields) {
     var template = '';
 
-    fields.forEach(function (item, number) {
+    fields.forEach(function(item, number) {
         if (number == 1) {
             template += createSearchInput(item, typeFields[item] == undefined ? "text" : typeFields[item]);
             template += '<button type="button" class="btn btn-success" data-toggle="collapse" data-target="#demo" aria-expanded="true">Advanced search &gt;&gt;</button>'
@@ -29,33 +30,17 @@ function loadSearchTemplate(fields) {
 
     });
 
-    $("#" + search_id).html(template);
+    $("#"+search_id).html(template);
 }
 
 function createSearchInput(name, type) {
-    input = '<div class="form-group"> <label for=' + name + '><b>Search by subject :</b></label>';
-<
-    input
-    type = "text"
-class
-    = "form-control"
-    id = "usr"
-    placeholder = "..." >
-        < div
-class
-    = "input-group-btn" >
-        < button
-    type = "button"
-class
-    = "btn btn-success"
-    data - toggle = "collapse"
-    data - target = "#demo"
-    aria - expanded = "true" > Advanced
-    search &gt;&gt; < /button>
-    < /div>
-    < /div>';
+    input = '<div class="form-group"> <label for='+name+'><b>Search by subject :</b></label>';
+          <input type="text" class="form-control" id="usr" placeholder="...">
+          <div class="input-group-btn">
+      <button type="button" class="btn btn-success" data-toggle="collapse" data-target="#demo" aria-expanded="true">Advanced search &gt;&gt;</button>
+          </div>
+        </div>';
 }
-
 
 waitJQuery(loadSearchTemplate, fields);
 
