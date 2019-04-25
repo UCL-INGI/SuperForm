a57d60ece5bac0435cf15a81159301b11f823517
AdriLezaack <adrien.lezaack@student.uclouvain.be>
2019-04-25 17:47:58 +0200

    Nouvel tentative de correction du rss

--- superform/superform/plugins/_rss_page.py
+++ superform/superform/plugins/_rss_page.py
@@ -3,7 +3,7 @@ import io
 import os
 
 from datetime import datetime
-from flask import Blueprint, render_template, send_from_directory
+from flask import Blueprint, render_template, send_from_directory, send_file
 from pathlib import Path
 from rfeed import *
 from superform.models import db, Channel, Post, Publishing
@@ -48,12 +48,20 @@ def display_rss_feed(id):
         items=items)
 
     generated_file = feed.rss()
-    file_name = str(c.id) + ".xml"
-    file_path = Path("superform/plugins/rss/" + file_name)
-    if Path(file_path).exists():
-        os.remove(file_path)
-    with open(file_path, "w+") as file:
-        file.write(generated_file)
-    file.close()
 
-    return send_from_directory("plugins/rss/", file_name, as_attachment=True, attachment_filename="feed.xml")
+
+    proxy = io.StringIO()
+    proxy.write(generated_file)
+
+    mem = io.BytesIO()
+    mem.write(proxy.getvalue().encode('utf-8'))
+
+    mem.seek(0)
+    proxy.close()
+
+    return send_file(
+        mem,
+        as_attachment=True,
+        attachment_filename='feed.xml',
+        mimetype='text/xml'
+    )
